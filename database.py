6900da178665331c6ca4978a5c969061be4917ef
luisteman <albertyto83@gmail.com>
2026-06-18 10:44:18 -0600

    Fix: Agregar pool pooling para mantener viva la conexion con Neon

--- database.py
+++ database.py
@@ -9,8 +9,12 @@ load_dotenv()
 # 2. Obtenemos la URL de conexión
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 3. Creamos el motor de conexión
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# 3. Creamos el motor de conexión CON LOS SEGUROS PARA NEON
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,  # Verfica si la conexión sigue viva antes de usarla
+    pool_recycle=300     # Renueva la conexión automáticamente cada 5 minutos
+)
 
 # 4. Creamos la fábrica de sesiones
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
